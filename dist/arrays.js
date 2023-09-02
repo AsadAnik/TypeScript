0317b53c81698daa26783c168fca46f144f2d948
AsadAnik <s.engineer63@gmail.com>
2023-09-02 12:30:37 +0600

    Updates with other codes

--- dist/arrays.js
+++ dist/arrays.js
@@ -4,6 +4,21 @@ function default_1() {
     console.log('========== Arrays ==========');
     const numbers = [1, 2, 3, 4, 5];
     console.log(numbers);
+    let studentGrade = [
+        { subject: "English", score: 120 },
+        { subject: "Math", score: 150 }
+    ];
+    console.log("Students Grade -- ", studentGrade);
+    const shoppingCartItems = [
+        { productId: "abc123", name: "iPhone Xs", price: 330, quantity: 12 },
+        { productId: "xyz123", name: "Samsung Galaxy S10", price: 250, quantity: 10 },
+    ];
+    console.log("Shopping Cart -- ", shoppingCartItems);
+    let employees = [
+        { id: 1, name: "Alice", position: "Manager", salary: 60000 },
+        { id: 2, name: "Bob", position: "Developer", salary: 50000 }
+    ];
+    console.log("Employee list -- ", employees);
 }
 exports.default = default_1;
 //# sourceMappingURL=arrays.js.map
--- src/arrays.ts
+++ src/arrays.ts
@@ -3,4 +3,48 @@ export default function (): void {
     // Numbers Array..
     const numbers: number[] = [1, 2, 3, 4, 5];
     console.log(numbers);
+
+    // Some real-life examples..
+    // Students Grades..
+    interface Grade {
+        subject: string;
+        score: number;
+    }
+
+    let studentGrade: Grade[] = [
+        { subject: "English", score: 120 },
+        { subject: "Math", score: 150 }
+    ];
+
+    console.log("Students Grade -- ", studentGrade);
+
+    // Shopping Cart..
+    interface CartItem {
+        productId: string;
+        name: string;
+        price: number;
+        quantity: number;
+    }
+
+    const shoppingCartItems: CartItem[] = [
+        { productId: "abc123", name: "iPhone Xs", price: 330, quantity: 12 },
+        { productId: "xyz123", name: "Samsung Galaxy S10", price: 250, quantity: 10 },
+    ];
+
+    console.log("Shopping Cart -- ", shoppingCartItems);
+
+    // Employee 
+    interface Employee {
+        id: number;
+        name: string;
+        position: string;
+        salary: number;
+    }
+    
+    let employees: Employee[] = [
+        { id: 1, name: "Alice", position: "Manager", salary: 60000 },
+        { id: 2, name: "Bob", position: "Developer", salary: 50000 }
+    ];
+
+    console.log("Employee list -- ", employees);
 }
